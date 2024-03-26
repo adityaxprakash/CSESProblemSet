c1f2f63fb309998e4d87c8b5cf95f25d149bc1af
adityaxprakash <cs1221595@cse.iitd.ac.in>
2024-03-26 20:57:00 +0530

    added graphs section

---
+++ GraphProblems/Counting_Rooms.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define F first
+#define S second
+#define pb push_back
+#define vi vector<int>
+#define all(x) (x).begin(),(x).end()
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    return 0;
+}
